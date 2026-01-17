3fee830563c555fd52354bcdab3de2983f46dc89
João Bezerra <joaobzzr@gmail.com>
2026-01-17 11:43:37 -0300

    feat(plugin): Add default keymaps for core features
    This commit introduces default, buffer-local keymaps for all core evaluation and REPL commands, which are automatically activated for Lisp files via a FileType autocommand.
    
    - Evaluation commands are mapped under the <leader>e mnemonic:
      - <leader>ee: Evaluate Enclosing form
      - <leader>er: Evaluate Root/Outermost form
      - <leader>eb: Evaluate Buffer
    
    - REPL commands are mapped under the <leader>r mnemonic:
      - <leader>rt: Toggle REPL window
      - <leader>rc: Clear REPL window

--- plugin/ooze.lua
+++ plugin/ooze.lua
@@ -11,3 +11,24 @@ vim.api.nvim_create_user_command("OozeEvalOutermost", ooze.eval_outermost, {})
 vim.api.nvim_create_user_command("OozeEvalBuffer", ooze.eval_buffer, {})
 vim.api.nvim_create_user_command("OozeReplToggle", repl.toggle, {})
 vim.api.nvim_create_user_command("OozeReplClear", repl.clear, {})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "lisp",
+	callback = function(args)
+		local opts = { buffer = args.buf, silent = true }
+
+		-- EVALUATION (Mnemonic: 'e')
+		-- <leader>ee -> Eval Enclosing
+		vim.keymap.set("n", "<leader>ee", ooze.eval_enclosing, opts)
+		-- <leader>er -> Eval Root/Outermost
+		vim.keymap.set("n", "<leader>er", ooze.eval_outermost, opts)
+		-- <leader>eb -> Eval Buffer
+		vim.keymap.set("n", "<leader>eb", ooze.eval_buffer, opts)
+
+		-- REPL (Mnemonic: 'r')
+		-- <leader>rt -> Repl Toggle
+		vim.keymap.set("n", "<leader>rt", repl.toggle, opts)
+		-- <leader>rc -> Repl Clear
+		vim.keymap.set("n", "<leader>rc", repl.clear, opts)
+	end,
+})
